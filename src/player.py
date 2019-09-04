46c1ceaf3b1cff70af5a17eee282063d0f3d4803
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-09-04 15:44:46 -0500

    re wrote room and player classes

--- src/player.py
+++ src/player.py
@@ -3,9 +3,9 @@
 
 
 class Player:
-    def __init__(self, name, current_room):
-        self.name = name
-        self.current_room = current_room
+# currently.	  def __init__(self, name, current_room):
+    self.name = name
+    self.current_room = current_room
 
-    def __str__(self):
-        return f"Name: {self.name}\nCurrent Room: {self.current_room}"
+   def __str__(self):
+    return f"{self.name}, {self.current_room}"
--- src/room.py
+++ src/room.py
@@ -3,13 +3,13 @@
 
 
 class Room:
-    def __init__(self, name, description, n_to, s_to, e_to, w_to):
-        self.name = name
-        self.description = description
-        self.n_to = n_to
-        self.s_to = s_to
-        self.e_to = e_to
-        self.w_to = w_to
+  def __init__(self, name, description, n_to=None, s_to=None, e_to=None, w_to=None):
+    self.name = name
+    self.description = description
+    self.n_to = n_to
+    self.s_to = s_to
+    self.e_to = e_to
+    self.w_to = w_to
 
-    def __str__(self):
-        return f"Name: {self.name}\n Description: {self.description}"
+   def __str__(self):
+    return "{}, {}".format(self.name, self.description)
